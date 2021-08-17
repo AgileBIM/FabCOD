4ced4297181aca6dcdee2af90ef868e9d74e42bb
Josh <josh.howard@ifnullthen.com>
2021-08-17 02:19:01 -0700

    reconfigure build for CICD

--- configurations/pack.py
+++ configurations/pack.py
@@ -1,34 +1,35 @@
+# import subprocess
+# import shutil
+# import errno
+# import shlex
+# import platform
+# import json
 import os
-import subprocess
 import os.path
-import shutil
-import errno
-import shlex
-import platform
 import sys
-import json
 from copyFiles import copyDimOptionChoices, copyExecuteInAcad, copyFabDataJson, copySnippetDefs
 
 def makepackage_vsix():    
-    with open('package.json', 'r') as jfile:
-        data = jfile.read()
-    jobj = json.loads(data)
-    suffix = jobj['version']
-    vsixPath = os.path.join(os.path.curdir, 'util', 'Builds', 'agilebim.fabcod-' + suffix + '.vsix')
+    # with open('package.json', 'r') as jfile:
+    #     data = jfile.read()
+    # jobj = json.loads(data)
+    # suffix = jobj['version']
+    # vsixPath = os.path.join(os.path.curdir, 'util', 'Builds', 'agilebim.fabcod-' + suffix + '.vsix')
+    vsixPath = os.path.join(os.path.curdir, 'agilebim.fabcod.vsix')
     print("===============================================")
     if (os.path.exists(vsixPath)):
         os.remove(vsixPath)
-        print("Deleted stale fabcod.vsix")
+        print("Deleted stale agilebim.fabcod.vsix")
 
     print("start to make vsix package")
     vsce = os.path.join(os.path.curdir, 'node_modules', '.bin', 'vsce')
     output_opt = " -o " + vsixPath
     os.system(vsce + " package" + output_opt) # nosec
     if (os.path.exists(vsixPath)):
-        print("It created fabcod.vsix file sucessfully")
+        print("It created agilebim.fabcod.vsix file sucessfully")
         ret = 0
     else:
-        print("It failed to create fabcod.vsix file")
+        print("It failed to create agilebim.fabcod.vsix file")
         ret = 1
     print("end make visx file")
     print("===============================================")
